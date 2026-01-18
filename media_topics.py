119d096084864bcc6281342eff8e77b930ad5575
peterjakubowski <97050386+peterjakubowski@users.noreply.github.com>
2026-01-17 16:50:29 -0800

    Create media_topics.py

---
+++ media_topics.py
@@ -0,0 +1,40 @@
+import json
+import os
+import requests
+
+
+MEDIATOPICS_URL = "https://cv.iptc.org/newscodes/mediatopic?lang=en-US&format=json"
+
+MEDIATOPICS_PATH = "./schema/mediatopic_cptall-en-US.json"
+
+
+# Function to download the Media Topics JSON file
+def download_mediatopics_json():
+    try:
+        # request media topics
+        response = requests.get(MEDIATOPICS_URL)
+        # check if the request was successful
+        response.raise_for_status()
+        # parse the JSON content into a dictionary
+        data = response.json()
+        # create the schema directory if it doesn't exist
+        os.makedirs("./schema", exist_ok=True)
+        # write the data to the JSON file
+        with open(MEDIATOPICS_PATH, 'w') as f:
+            json.dump(data, f, indent=4)
+
+    except requests.exceptions.RequestException as e:
+        print(f"Error during request: {e}")
+    except json.JSONDecodeError as e:
+        print(f"Error decoding JSON: {e}")
+    except IOError as e:
+        print(f"Error writing to file: {e}")
+
+
+# Download the Media Topics JSON file if it doesn't exist
+if not os.path.exists(MEDIATOPICS_PATH):
+    print("Downloading Media Topics Controlled Vocabulary from IPTC web")
+    download_mediatopics_json()
+# Load the Media Topics JSON file
+with open(MEDIATOPICS_PATH, "r") as file:
+    media_topics = json.load(file)
